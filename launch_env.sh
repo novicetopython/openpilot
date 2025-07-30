108d81f4c6f6a33927d0691b7ef5c40eed7b62b1
novicetopython <35001563+novicetopython@users.noreply.github.com>
2025-07-30 12:31:18 +0100

    Update launch_env.sh

--- launch_env.sh
+++ launch_env.sh
@@ -5,6 +5,9 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+export ATHENA_HOST='wss://athena.konik.ai'
+export API_HOST='https://api.konik.ai'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="10.1"
