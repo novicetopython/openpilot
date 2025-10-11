5a51d24ac1d648d1c407b9a8ed650b1c55a94a95
Mr.one <mawei1991@gmail.com>
2025-10-11 19:42:35 +0800

    编辑manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -29,7 +29,6 @@ def manager_init() -> None:
   build_metadata = get_build_metadata()
 
   params = Params()
-  params.put("LanguageSetting", "main_zh-CHT")
   params.clear_all(ParamKeyFlag.CLEAR_ON_MANAGER_START)
   params.clear_all(ParamKeyFlag.CLEAR_ON_ONROAD_TRANSITION)
   params.clear_all(ParamKeyFlag.CLEAR_ON_OFFROAD_TRANSITION)
