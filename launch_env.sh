869a0004979533fce87cdd028614c69f36e20915
Mr.one <mawei1991@gmail.com>
2025-10-26 22:35:50 +0800

    编辑launch_env.sh

--- launch_env.sh
+++ launch_env.sh
@@ -5,7 +5,8 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
-export ATHENA_HOST='wss://athena.konik.ai'
+export ATHENA_HOST='ws://server.mr-one.cn:7899'
+export API_HOST='http://server.mr-one.cn:7898'
 if [ -s /data/params/d/dp_device_model_selected ]; then
   export FINGERPRINT="$(cat /data/params/d/dp_device_model_selected)"
 fi
